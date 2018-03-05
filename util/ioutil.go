5de2670af91f2f0f612de06b6ddaaad545ee60dd
Bill Zissimopoulos <billziss@navimatics.com>
2018-03-04 21:38:37 -0800

    add util package

---
+++ util/ioutil.go
@@ -0,0 +1,76 @@
+/*
+ * ioutil.go
+ *
+ * Copyright 2018 Bill Zissimopoulos
+ */
+/*
+ * This file is part of golib.
+ *
+ * It is licensed under the MIT license. The full license text can be found
+ * in the License.txt file at the root of this project.
+ */
+
+package util
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func ReadFunc(path string, fn func(*os.File) (interface{}, error)) (data interface{}, err error) {
+	file, err := os.OpenFile(path, os.O_RDONLY, 0)
+	if nil != err {
+		return
+	}
+	defer func() {
+		file.Close()
+	}()
+
+	data, err = fn(file)
+	return
+}
+
+func ReadData(path string) (data interface{}, err error) {
+	return ReadFunc(path, func(file *os.File) (interface{}, error) {
+		return ioutil.ReadAll(file)
+	})
+}
+
+func WriteFunc(path string, perm os.FileMode, fn func(*os.File) error) (err error) {
+	var r [10]byte
+	_, err = rand.Read(r[:])
+	if nil != err {
+		return
+	}
+
+	newpath := path + hex.EncodeToString(r[:])
+	file, err := os.OpenFile(newpath, os.O_CREATE|os.O_EXCL|os.O_RDWR, perm)
+	if nil != err {
+		return
+	}
+	defer func() {
+		file.Close()
+		if nil == err {
+			err = os.Rename(newpath, path)
+		}
+		if nil != err {
+			os.Remove(newpath)
+		}
+	}()
+
+	err = fn(file)
+	return
+}
+
+func WriteData(path string, perm os.FileMode, data []byte) (err error) {
+	return WriteFunc(path, perm, func(file *os.File) error {
+		n, err := file.Write(data)
+		if nil == err && n < len(data) {
+			err = io.ErrShortWrite
+		}
+		return err
+	})
+}
---
+++ util/ioutil_test.go
@@ -0,0 +1,40 @@
+/*
+ * ioutil_test.go
+ *
+ * Copyright 2018 Bill Zissimopoulos
+ */
+/*
+ * This file is part of golib.
+ *
+ * It is licensed under the MIT license. The full license text can be found
+ * in the License.txt file at the root of this project.
+ */
+
+package util
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestReadWriteData(t *testing.T) {
+	path := filepath.Join(os.TempDir(), "util_test")
+	os.Remove(path)
+	defer os.Remove(path)
+
+	s := "hello, world"
+	err := WriteData(path, 0644, ([]byte)(s))
+	if nil != err {
+		t.Error(err)
+	}
+
+	b, err := ReadData(path)
+	if nil != err {
+		t.Error(err)
+	}
+
+	if s != string(b.([]byte)) {
+		t.Error()
+	}
+}
