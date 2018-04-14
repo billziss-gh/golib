357d83f09981ac00887f5629f95c2a75eaf6c4bc
Bill Zissimopoulos <billziss@navimatics.com>
2018-04-14 14:15:15 -0700

    tools: mkdoc.sh: now works when vendored

--- _tools/mkdoc.sh
+++ _tools/mkdoc.sh
@@ -3,21 +3,22 @@
 # requires https://github.com/davecheney/godoc2md
 
 golib="github.com/billziss-gh/golib"
-progdir=$(dirname "$0")
 
+cd $(dirname "$0")/..
 (
     sed -n '1,/(GODOC)/p' README.md
-    go list -f "{{.ImportPath}} {{.Doc}}" "$golib/..." | sed 's@.*/vendor/@@' |
+    go list -f "{{.ImportPath}} {{.Doc}}" ./... | sed 's@.*/vendor/@@' |
     while read p d; do
         echo "- [$(basename $p)](#$p) - $d"
     done
-    go list "$golib/..." | sed 's@.*/vendor/@@' |
+    go list ./... |
     while read p; do
-        $GOPATH/bin/godoc2md -template "$progdir/godoc2md.templ" $p |
+        $GOPATH/bin/godoc2md -template _tools/godoc2md.templ $p |
         sed \
+            -e "s@github.com/[^/]*/[^/]*/vendor/@@g"\
             -e "s@/src/$golib/@@g"\
             -e "s@/src/target@$(basename $p)@g"\
             -e "s@?s=[0-9][0-9]*:[0-9][0-9]*#@#@g"
     done
-) > $progdir/../README.md.new
-mv $progdir/../README.md.new $progdir/../README.md
+) > README.md.new
+mv README.md.new README.md
