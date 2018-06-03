be9c2b6e245a33af835beb0b60700c729305d01f
Bill Zissimopoulos <billziss@navimatics.com>
2018-06-02 18:13:10 -0700

    shlex: improved testing

--- shlex/shlex_test.go
+++ shlex/shlex_test.go
@@ -14,6 +14,7 @@ package shlex
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -86,6 +87,84 @@ func TestPosix(t *testing.T) {
 	}
 }
 
+func TestPosix2(t *testing.T) {
+	// See cpython/Lib/test/test_shlex.py (branch 2.7)
+	tests := `x|x|
+foo bar|foo|bar|
+ foo bar|foo|bar|
+ foo bar |foo|bar|
+foo   bar    bla     fasel|foo|bar|bla|fasel|
+x y  z              xxxx|x|y|z|xxxx|
+\x bar|x|bar|
+\ x bar| x|bar|
+\ bar| bar|
+foo \x bar|foo|x|bar|
+foo \ x bar|foo| x|bar|
+foo \ bar|foo| bar|
+foo "bar" bla|foo|bar|bla|
+"foo" "bar" "bla"|foo|bar|bla|
+"foo" bar "bla"|foo|bar|bla|
+"foo" bar bla|foo|bar|bla|
+foo 'bar' bla|foo|bar|bla|
+'foo' 'bar' 'bla'|foo|bar|bla|
+'foo' bar 'bla'|foo|bar|bla|
+'foo' bar bla|foo|bar|bla|
+blurb foo"bar"bar"fasel" baz|blurb|foobarbarfasel|baz|
+blurb foo'bar'bar'fasel' baz|blurb|foobarbarfasel|baz|
+""||
+''||
+foo "" bar|foo||bar|
+foo '' bar|foo||bar|
+foo "" "" "" bar|foo||||bar|
+foo '' '' '' bar|foo||||bar|
+\"|"|
+"\""|"|
+"foo\ bar"|foo\ bar|
+"foo\\ bar"|foo\ bar|
+"foo\\ bar\""|foo\ bar"|
+"foo\\" bar\"|foo\|bar"|
+"foo\\ bar\" dfadf"|foo\ bar" dfadf|
+"foo\\\ bar\" dfadf"|foo\\ bar" dfadf|
+"foo\\\x bar\" dfadf"|foo\\x bar" dfadf|
+"foo\x bar\" dfadf"|foo\x bar" dfadf|
+\'|'|
+'foo\ bar'|foo\ bar|
+'foo\\ bar'|foo\\ bar|
+"foo\\\x bar\" df'a\ 'df"|foo\\x bar" df'a\ 'df|
+\"foo|"foo|
+\"foo\x|"foox|
+"foo\x"|foo\x|
+"foo\ "|foo\ |
+foo\ xx|foo xx|
+foo\ x\x|foo xx|
+foo\ x\x\"|foo xx"|
+"foo\ x\x"|foo\ x\x|
+"foo\ x\x\\"|foo\ x\x\|
+"foo\ x\x\\""foobar"|foo\ x\x\foobar|
+"foo\ x\x\\"\'"foobar"|foo\ x\x\'foobar|
+"foo\ x\x\\"\'"fo'obar"|foo\ x\x\'fo'obar|
+"foo\ x\x\\"\'"fo'obar" 'don'\''t'|foo\ x\x\'fo'obar|don't|
+"foo\ x\x\\"\'"fo'obar" 'don'\''t' \\|foo\ x\x\'fo'obar|don't|\|
+'foo\ bar'|foo\ bar|
+'foo\\ bar'|foo\\ bar|
+foo\ bar|foo bar|
+:-) ;-)|:-)|;-)|
+áéíóú|áéíóú|`
+
+	for i, l := range strings.Split(tests, "\n") {
+		l = l[:len(l)-1]
+		p := strings.Split(l, "|")
+
+		test_i := p[0]
+		test_o := p[1:]
+
+		o := Posix.Split(test_i)
+		if !reflect.DeepEqual(test_o, o) {
+			t.Errorf("(%v) expect %#v, got %#v", i, test_o, o)
+		}
+	}
+}
+
 func TestWindows(t *testing.T) {
 	tests := []struct {
 		i string
@@ -135,7 +214,22 @@ func TestWindows(t *testing.T) {
 		{`"foo\\\   bar"`, []string{`foo\\\   bar`}},
 		{`"foo\\\\   bar"`, []string{`foo\\\\   bar`}},
 		{`"foo\\\\\   bar"`, []string{`foo\\\\\   bar`}},
+	}
 
+	for _, test := range tests {
+		o := Windows.Split(test.i)
+		if !reflect.DeepEqual(test.o, o) {
+			t.Errorf("expect %#v, got %#v", test.o, o)
+		}
+	}
+}
+
+func TestWindows2(t *testing.T) {
+	// See https://tinyurl.com/ycdj5ghh
+	tests := []struct {
+		i string
+		o []string
+	}{
 		{`CallMeIshmael`, []string{`CallMeIshmael`}},
 		{`"Call Me Ishmael"`, []string{`Call Me Ishmael`}},
 		{`Cal"l Me I"shmael`, []string{`Call Me Ishmael`}},
